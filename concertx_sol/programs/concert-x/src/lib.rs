2fe160c7666bec15c2a0d200a6377acfa7789662
Jubel Correa <jubel.correa@gmail.com>
2024-12-14 22:33:14 -0500

    Finished to create concert on solana

--- concertx_sol/programs/concert-x/src/lib.rs
+++ concertx_sol/programs/concert-x/src/lib.rs
@@ -6,11 +6,55 @@ declare_id!("Fh63wv5yhjeNPhyd7jN4ZAhAqLjngHxr8fhV9u7F21fu");
 pub mod concert_x {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_concert(
+        ctx: Context<CreateConcert>,
+        title: String,
+        description: String,
+        goal_amount: u32,
+        start_date: i64,
+        end_date: i64,
+    ) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let concert = &mut ctx.accounts.concert;
+        concert.creator = ctx.accounts.initializer.key();
+        concert.title = title;
+        concert.description = description;
+        concert.goal_amount = goal_amount;
+        concert.start_date = start_date;
+        concert.end_date = end_date;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String)]
+pub struct CreateConcert<'info> {
+    #[account(
+        init,
+        seeds = [b"concertX", initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = DISCRIMINATOR + Concert::INIT_SPACE
+    )]
+    pub concert: Account<'info, Concert>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Concert {
+    pub creator: Pubkey,  
+    #[max_len(20)]            // Creator's wallet address
+    pub title: String,                // Campaign title
+    #[max_len(30)]
+    pub description: String,          // Campaign description
+    pub goal_amount: u32,             // Funding goal in lamports
+    pub current_amount: u64,          // Current amount pledged
+    pub start_date: i64,              // Campaign start time
+    pub end_date: i64,                // Campaign end time
+    pub status: u8,                   // 0 = active, 1 = completed, 2 = cancelled
+}
+
+const DISCRIMINATOR: usize = 8;
--- concertx_sol/tests/concert-x.ts
+++ concertx_sol/tests/concert-x.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ConcertX } from "../target/types/concert_x";
+import { expect } from "chai";
+import BN from "bn.js";
 
 describe("concert-x", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.ConcertX as Program<ConcertX>;
+  const [concertXPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("concertX"), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  );
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const title = "The big concert";
+    
+    const desc = "The most amazing concert";
+    const goalAmount = 1000;
+    const startDate = new BN(new Date(2024,12,31,10,0,0).getTime());
+    const endDate = startDate.add(new BN(1000));
+    const tx = await program.methods.createConcert(title, desc,goalAmount,startDate,endDate).rpc();
     console.log("Your transaction signature", tx);
+    const account = await program.account.concert.fetch(concertXPda);
+    expect(account.title).equals(title);
+    expect(account.description).equals(desc);
+    expect(account.startDate.eq(startDate)).to.be.true;
+    expect(account.endDate.eq(endDate)).to.be.true;
   });
 });
